330a2735a32029d4e05d9d15420bc4316da18ebf
aka <aaronkaltman@gmail.com>
2023-12-13 05:40:22 -0500

    adding in stats

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,11 +1,23 @@
 import TextArea from "./TextArea";
 import Stats from "./Stats";
+import { useState } from "react";
 
 export default function Container() {
+  const [text, setText] = useState("");
+  const numberOfCharacters = text.length;
+  const numberOfWords = text.split(/\s+/).filter((word) => word !== "").length;
+  const instagram = 280;
+  const facebook = 2200;
+
   return (
     <main className="container">
-      <TextArea />
-      <Stats />
+      <TextArea text={text} setText={setText} />
+      <Stats
+        numberOfCharacters={numberOfCharacters}
+        numberOfWords={numberOfWords}
+        instagram={instagram}
+        facebook={facebook}
+      />
     </main>
   );
 }
--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,24 @@
-export default function Stats() {
-  return <section className="stats">Stats</section>;
+export default function Stats({
+  numberOfCharacters,
+  numberOfWords,
+  instagram,
+  facebook,
+}) {
+  return (
+    <section className="stats">
+      <Stat number={numberOfWords} heading="Words" />
+      <Stat number={numberOfCharacters} heading="Characters" />
+      <Stat number={instagram - numberOfCharacters} heading="Instagram" />
+      <Stat number={facebook - numberOfCharacters} heading="Facebook" />
+    </section>
+  );
+}
+
+function Stat({ number, heading }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <h2 className="second-heading">{heading}</h2>
+    </section>
+  );
 }
