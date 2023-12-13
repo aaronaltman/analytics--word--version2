cd60da71044feb2a451bfff6b67ba6add86d151e
aka <aaronkaltman@gmail.com>
2023-12-13 01:05:49 -0500

    not allowing script tags

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 export default function TextArea() {
+  const [text, setText] = useState("");
+
   return (
     <textarea
       onChange={(e) => {
-        let text = e.target.value;
-        let textArray = text.split(" ");
-        console.log(text);
+        const newText = e.target.value;
+        setText(newText);
+        if (newText.includes("<script>")) {
+          alert("No script tag allowed");
+          setText("");
+        }
       }}
+      value={text}
       className="textarea"
       placeholder="Enter your text here"
       spellCheck="false"
